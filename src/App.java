26ab58f5af1a5284613d5e0fe57636f11d8aa81b
Álvaro Jordão <alvccpj@gmail.com>
2023-03-01 12:22:16 -0300

    1st commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int n1, n2;
+        System.out.println("Programa para saber se o primeiro número é divisível pelo segundo!");
+        System.out.print("Informe o primeiro número: ");
+        n1 = in.nextInt();
+        System.out.print("Informe o segundo número: ");
+        n2 = in.nextInt();
+        function(n1, n2);
+
+    }
+
+    public static boolean function(int n1, int n2) {
+        if (n1 % n2 == 0) {
+            System.out.println("O primeiro número é divisível pelo segundo número!");
+            return true;
+        } else {
+            System.out.println("O primeiro número não é divisível pelo segundo número!");
+            return false;
+        }
+    }
+}
