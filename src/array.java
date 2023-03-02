27dfc03fbc92abc0cfaef69051deb840dc631428
Álvaro Jordão <alvccpj@gmail.com>
2023-03-02 11:59:42 -0300

    2nd commit

---
+++ src/array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double vet[] = new double[10];
+        double soma = 0;
+        double media;
+
+        for (int i = 0; i < vet.length; i++) {
+            System.out.println("Calcule a média de 10 notas");
+            System.out.print("Informe a sua " + (i + 1) + " nota: ");
+            vet[i] = in.nextDouble();
+        }
+
+        for (int i = 0; i < vet.length; i++) {
+            soma = soma + vet[i];
+        }
+
+        for (int i = 0; i < vet.length; i++) {
+            soma = soma + vet[i];
+        }
+
+        media = soma / 10;
+        System.out.println("Media: " + media);
+    }
+}
--- src/divisible.java
+++ src/divisible.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class divisible {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         int n1, n2;
