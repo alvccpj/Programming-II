d3bd5bf7b35ba5083ac0f59f833c32072bb9d7b7
Álvaro Jordão <alvccpj@gmail.com>
2023-03-08 11:52:03 -0300

    solving arrays exercises

---
+++ src/ex1.java
@@ -0,0 +1,5 @@
+public class ex1 {
+    public static void main(String[] args) {
+        int vet [] = new int [10];
+    }
+}
---
+++ src/ex4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+        int array[] = new int[10];
+        int maiorNumero = buscaMaior(array);
+
+        System.out.println("A posição do maior elemento é de índice " + maiorNumero);
+
+    }
+
+    public static int buscaMaior(int array[]) {
+        Scanner in = new Scanner(System.in);
+        int maior = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Informe o elemento de índice " + i + ": ");
+            array[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > array[maior]) {
+                maior = i;
+            }
+        }
+
+        return maior;
+    }
+
+}
